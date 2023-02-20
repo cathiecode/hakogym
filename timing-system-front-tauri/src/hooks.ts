8b3bdc59552adc8b181b3b26df7f90c5eef3f012
cathiecode <cathiecode@gmail.com>
2023-02-20 10:36:48 +0900

    remove supabase

--- timing-system-front-tauri/src/hooks.ts
+++ timing-system-front-tauri/src/hooks.ts
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import { getEntryList } from "./api";
 
 export function useEntryList(competitionId: string) {
   return useQuery(["entryList", competitionId], () => getEntryList(competitionId));
