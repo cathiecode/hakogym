859e1b6154a2753407074706ef0327556cef27fd
cathiecode <cathiecode@gmail.com>
2023-02-23 10:30:04 +0900

    fix windows build pipeline

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 
-import {spawnSync} from "node:child_process";
-
+import { spawnSync } from "node:child_process";
 
 const run = (cwd, command, args) => {
   console.log("Running", command, args);
-  spawnSync(command, args, {cwd, stdio: "inherit"});
-}
+  console.log(spawnSync(command, args, { cwd, stdio: "inherit", shell: true }));
+};
 
-run("timing-system", "cargo", [
-  "build",
-  "--release",
-  "--target=x86_64-pc-windows-gnu",
-]); 
-run("timing-system-front-tauri", "npm", ["run", "build"]); 
-run("timing-system-google-spreadsheet-exporter", "npm", ["run", "build"])
+run("timing-system", "cargo", ["build", "--release"]);
+run("timing-system-front-tauri", "npm", ["run", "build"]);
+run("timing-system-google-spreadsheet-exporter", "npm", ["run", "build"]);
