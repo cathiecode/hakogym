fd4da272e457cb027eec1cea11b89607829fc39f
cathiecode <cathiecode@gmail.com>
2023-03-21 18:55:18 +0900

    update build script

--- scripts/build.js
+++ scripts/build.js
@@ -13,7 +13,9 @@ const run = (cwd, command, args) => {
 
 
 run("timing-system", "cargo", ["build", "--release"]);
+run("timing-system-signal-io", "cargo", ["build", "--release"]);
 run("timing-system-front-tauri", "npm", ["run", "build"]);
+run("timing-system-vlc-controller", "npm", ["run", "build"]);
 run("timing-system-google-spreadsheet-exporter", "npm", ["run", "build"]);
 run("timing-system-launcher", "npm", ["run", "build"]);
 
@@ -22,11 +24,20 @@ mkdirSync("build");
 mkdirSync(path.join("build", "data"));
 
 copySync(path.join("timing-system", "target", "release", "timing-system.exe"), path.join("build", "data", "timing-system.exe"));
+copySync(path.join("timing-system-signal-io", "target", "release", "timing-system-signal-io.exe"), path.join("build", "data", "timing-system-signal-io.exe"));
 copySync(path.join("timing-system-front-tauri", "src-tauri", "target", "release", "timing-system-front.exe"), path.join("build", "data", "timing-system-front-tauri.exe"));
+
 copySync(path.join("timing-system-google-spreadsheet-exporter", "build"), path.join("build", "data", "timing-system-google-spreadsheet-exporter"));
 copySync(path.join("timing-system-google-spreadsheet-exporter", "package.json"), path.join("build", "data", "timing-system-google-spreadsheet-exporter", "package.json"));
+
+copySync(path.join("timing-system-vlc-controller", "build"), path.join("build", "data", "timing-system-vlc-controller"));
+copySync(path.join("timing-system-vlc-controller", "package.json"), path.join("build", "data", "timing-system-vlc-controller", "package.json"));
+
 run(path.join("build", "data", "timing-system-google-spreadsheet-exporter"), "npm", ["install", "--omit=dev"]);
+run(path.join("build", "data", "timing-system-vlc-controller"), "npm", ["install", "--omit=dev"]);
+
 copySync(path.join("timing-system-launcher", "src-tauri", "target", "release", "timing-system-launcher.exe"), path.join("build", "timing-system-launcher.exe"));
 copySync(path.join("deps", "node18"), path.join("build", "data", "node18"));
+copySync(path.join("deps", "VLCPortable"), path.join("build", "data", "VLCPortable"));
 
 copySync(path.join("resources"), path.join("build", "resources"));
