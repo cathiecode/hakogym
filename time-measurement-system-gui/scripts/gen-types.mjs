a2f828bc4a9635198d683877e0cf5f5011d810de
cathiecode <cathiecode@gmail.com>
2023-04-11 16:00:44 +0900

    fix: generate proto

--- time-measurement-system-gui/scripts/gen-types.mjs
+++ time-measurement-system-gui/scripts/gen-types.mjs
@@ -1,4 +1,11 @@
 import { spawnSync } from "node:child_process";
+import {
+  appendFileSync,
+  readFileSync,
+  readdirSync,
+  writeFileSync,
+} from "node:fs";
+import { join } from "node:path";
 
 spawnSync(
   "npx",
@@ -12,6 +19,21 @@ spawnSync(
     "../proto/running_observer.proto",
     "../proto/records.proto",
     "../proto/aggrigated_change_broadcaster.proto",
+    "../proto/service_manager.proto",
   ],
   { stdio: "inherit", shell: true }
 );
+
+const dir = readdirSync(join("src", "types", "proto"), { withFileTypes: true });
+
+dir
+  .filter((item) => item.isFile && item.name.endsWith(".ts"))
+  .forEach((item) => {
+    const file = readFileSync(join("src", "types", "proto", item.name), {
+      encoding: "utf-8",
+    });
+    writeFileSync(
+      join("src", "types", "proto", item.name),
+      ["// @ts-nocheck", file].join("\n")
+    );
+  });
