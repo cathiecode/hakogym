a5e3dade78c7c64f464cc7027ba207631e6d5126
cathiecode <cathiecode@gmail.com>
2023-04-17 22:35:00 +0900

    fix: スプシヘルプを追加

--- time-measurement-system-gui/src/pages/SpreadSheet.tsx
+++ time-measurement-system-gui/src/pages/SpreadSheet.tsx
@@ -7,7 +7,16 @@ export default function SpreadSheet() {
     <Page>
       <Container fluid>
         <Alert variant="info">
-            書き出し先のシートのタイトルが<code>HAS_TIMING_SYSTEM</code>となっていることを確認してください
+          書き出し先のシートのタイトルが<code>HAS_TIMING_SYSTEM</code>
+          となっていることを確認してください (
+          <a
+            href="https://github.com/cathiecode/hakogym/wiki/%E8%A8%88%E6%B8%AC%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E3%81%AE%E4%BD%BF%E3%81%84%E6%96%B9#%E3%82%B9%E3%83%97%E3%83%AC%E3%83%83%E3%83%89%E3%82%B7%E3%83%BC%E3%83%88%E6%8E%A5%E7%B6%9A"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ヘルプ
+          </a>
+          )
         </Alert>
         <SpreadSheetStatus />
       </Container>
